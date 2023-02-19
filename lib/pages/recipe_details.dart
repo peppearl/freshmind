c9ab8fd1aa948d4e92788d2cfc32ac19a9283e18
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2023-02-19 10:07:17 +0100

    feat: retrieving ingredients list & adding style to recipe details page

--- lib/pages/recipe_details.dart
+++ lib/pages/recipe_details.dart
@@ -1,19 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:freshmind/components/app_bar_title.dart';
 import 'package:freshmind/models/recipe.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RecipeDetails extends StatelessWidget {
   final Recipe recipe;
-  const RecipeDetails({super.key, required this.recipe});
+  final List<dynamic> ingredients;
+  const RecipeDetails(
+      {super.key, required this.recipe, required this.ingredients});
 
   @override
   Widget build(BuildContext context) {
+    String cheap;
+    String vegan;
+    late List<dynamic> _ingredient_name;
+
+    //cost of the recipe
+    if (recipe.cheap) {
+      cheap = "Bon marché";
+    } else {
+      cheap = "Assez cher";
+    }
+
+    //is the recipe vegan ?
+    if (recipe.vegetarian) {
+      vegan = "Végétarien";
+    } else {
+      vegan = "";
+    }
+
     return Scaffold(
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           SafeArea(child: AppBarTitle(title: "Mes recettes".toUpperCase())),
-          Container(
+          /*Container(
             height: 200,
             decoration: BoxDecoration(
               image: DecorationImage(
@@ -25,16 +47,226 @@ class RecipeDetails extends StatelessWidget {
                   style: const TextStyle(
                       fontSize: 16, height: 1.5, color: Colors.white)),
             ),
+          ),*/
+          Flexible(
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  centerTitle: true,
+                  expandedHeight: 200,
+                  flexibleSpace: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(recipe.image), fit: BoxFit.cover),
+                    ),
+                    child: Center(
+                      child: Text(recipe.title,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontSize: 16, height: 1.5, color: Colors.white)),
+                    ),
+                  ),
+                ),
+                SliverList(
+                    delegate: SliverChildListDelegate([
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                          child: Container(
+                            padding: const EdgeInsets.all(10),
+                            width: 200,
+                            decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                                border: Border.all(
+                                    color: const Color(0xFFD9D9D9),
+                                    width: 1.5)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Wrap(
+                                  direction: Axis.vertical,
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  children: [
+                                    Text(
+                                      recipe.readyInMinutes.toString(),
+                                      style: GoogleFonts.poppins(
+                                          fontWeight: FontWeight.w700,
+                                          color: const Color.fromARGB(
+                                              255, 185, 124, 123)),
+                                    ),
+                                    const Text("minutes")
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Wrap(
+                                  direction: Axis.vertical,
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  children: [
+                                    Text(
+                                      recipe.servings.toString(),
+                                      style: GoogleFonts.poppins(
+                                          fontWeight: FontWeight.w700,
+                                          color: const Color.fromARGB(
+                                              255, 185, 124, 123)),
+                                    ),
+                                    const Text("personnes")
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Wrap(
+                          children: [
+                            Chip(label: Text(cheap)),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Chip(
+                              label: Text(recipe.difficulty),
+                            ),
+                          ],
+                        ),
+                        const Text("Ingrédients"),
+                      ],
+                    ),
+                  ),
+                ])),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) => Container(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Row(
+                        children: [
+                          Text(ingredients[index]["name"]
+                              .toString()
+                              .replaceAll(RegExp(r'[^\w\s]+'), '')),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Text(ingredients[index]['quantity'].toString()),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Text(ingredients[index]['unit']),
+                        ],
+                      ),
+                    ),
+                    childCount: ingredients.length,
+                  ),
+                ),
+              ],
+            ),
           ),
+          /*
           SingleChildScrollView(
             child: Container(
               padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [Text("Ingrédients pour ${recipe.servings}")],
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      width: 200,
+                      decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          border: Border.all(
+                              color: const Color(0xFFD9D9D9), width: 1.5)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Wrap(
+                            direction: Axis.vertical,
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            children: [
+                              Text(
+                                recipe.readyInMinutes.toString(),
+                                style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w700,
+                                    color: const Color.fromARGB(
+                                        255, 185, 124, 123)),
+                              ),
+                              const Text("minutes")
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Wrap(
+                            direction: Axis.vertical,
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            children: [
+                              Text(
+                                recipe.servings.toString(),
+                                style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w700,
+                                    color: const Color.fromARGB(
+                                        255, 185, 124, 123)),
+                              ),
+                              const Text("personnes")
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Wrap(
+                    children: [
+                      Chip(label: Text(cheap)),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Chip(
+                        label: Text(recipe.difficulty),
+                      ),
+                    ],
+                  ),
+                  const Text("Ingrédients"),
+                  Flexible(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: ingredients.length,
+                      itemBuilder: (context, index) {
+                        return Row(
+                          children: [
+                            //Text(ingredients[index]['name'].toString()),
+                            const SizedBox(
+                              width: 3,
+                            ),
+                            Text(ingredients[index]['quantity'].toString()),
+                            const SizedBox(
+                              width: 3,
+                            ),
+                            Text(ingredients[index]['unit']),
+                          ],
+                        );
+                      },
+                    ),
+                  )
+                ],
               ),
             ),
-          )
+          ) */
         ],
       ),
     );
--- lib/pages/recipes.dart
+++ lib/pages/recipes.dart
@@ -7,8 +7,6 @@ import 'package:freshmind/pages/recipe_details.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../icons/custom_icons_icons.dart';
-
 class Recipes extends StatefulWidget {
   const Recipes({super.key});
 
@@ -25,10 +23,14 @@ class _RecipesState extends State<Recipes> with SingleTickerProviderStateMixin {
 
   //to provide events in calendar
   late List<Recipe> _recipes;
+  late List<dynamic> _ingredients;
+  late List<dynamic> _ingredient_name;
 
   //get events from firebase
   _loadFirestoreEvents() async {
     _recipes = [];
+    _ingredients = [];
+    _ingredient_name = [];
 
     //get all events of current user
     final recipes = await FirebaseFirestore.instance
@@ -39,6 +41,38 @@ class _RecipesState extends State<Recipes> with SingleTickerProviderStateMixin {
         .get();
     final allRecipes = recipes.docs.map((doc) => doc.data()).toList();
     _recipes = allRecipes;
+
+    for (var doc in recipes.docs) {
+      final ingredients = doc["ingredients"].toList();
+      _ingredients = ingredients;
+      //print(_ingredients);
+
+      for (var i = 0; i < ingredients.length; i++) {
+        DocumentReference documentReference = FirebaseFirestore.instance
+            .collection("ingredients")
+            .doc(ingredients[i]["ingredient_id"]);
+        documentReference.get().then((datasnapshot) {
+          if (datasnapshot.exists) {
+            //print(datasnapshot.data());
+            //ingredients[i]["name"] = datasnapshot.data();
+            //print(datasnapshot.data());
+            Map<dynamic, dynamic>? map = datasnapshot.data() as Map?;
+            var test = map?.values.toList();
+            ingredients[i]["name"] = test;
+            print(ingredients[i]["name"]);
+          }
+        });
+
+        /*
+      documentReference.get().then((datasnapshot) {
+        if (datasnapshot.exists) {
+          print(datasnapshot.data().toString());
+        } else {
+          print("No such user");
+        }
+      });*/
+      }
+    }
 
     setState(() {});
   }
@@ -104,7 +138,8 @@ class _RecipesState extends State<Recipes> with SingleTickerProviderStateMixin {
                         ),
                       ),
                       onTap: () {
-                        Get.to(() => RecipeDetails(recipe: event));
+                        Get.to(() => RecipeDetails(
+                            recipe: event, ingredients: _ingredients));
                       });
                 }),
               ],
@@ -137,7 +172,8 @@ class _RecipesState extends State<Recipes> with SingleTickerProviderStateMixin {
                         ),
                       ),
                       onTap: () {
-                        Get.to(() => RecipeDetails(recipe: event));
+                        Get.to(() => RecipeDetails(
+                            recipe: event, ingredients: _ingredients));
                       });
                 }),
               ],
@@ -170,7 +206,8 @@ class _RecipesState extends State<Recipes> with SingleTickerProviderStateMixin {
                         ),
                       ),
                       onTap: () {
-                        Get.to(() => RecipeDetails(recipe: event));
+                        Get.to(() => RecipeDetails(
+                            recipe: event, ingredients: _ingredients));
                       });
                 }),
               ],
